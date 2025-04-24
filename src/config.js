3b154a7ff210c22c1218cdf5b65c380eee58d677
daitong <1015532604@qq.com>
2025-04-24 22:48:22 +0800

    优化

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // config.js
-//prod
-const baseUrl = 'dinner.daitong.xyz:60000' ;
-//dev
-// const baseUrl = 'localhost:60000' ;
+const baseUrl = process.env.NODE_ENV === 'production' 
+  ? 'dinner.daitong.xyz:60000'
+  : 'localhost:60000';
 
 export default baseUrl;
